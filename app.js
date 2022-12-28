d47318aa2b4eafdddb9c7204c9d3b3f1999b1e93
ripon13055 <mohiuddin.saifullah@gmail.com>
2022-12-29 00:00:45 +0600

    add brand model,router and cntroller

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
+const morgan = require("morgan");
 
-
-const productRouter = require('./routes/productRouter');
+const productRouter = require("./routes/productRouter");
+const brandrouter = require("./routes/brandeRouter");
 // const productController = require('./routes/productRouter');
 
-
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 
-app.use('/api/ecom/products', productRouter);
-
-
-
+app.use("/api/ecom/products", productRouter);
+app.use("/api/ecom/brand", brandrouter);
 
 module.exports = app;
--- model/ProductModel.js
+++ model/ProductModel.js
@@ -1,37 +1,43 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-    name:{
-        type: String,
-        required: [true, 'Product must have a name']
+  name: {
+    type: String,
+    required: [true, "Product must have a name"],
+  },
+  price: {
+    type: Number,
+    required: [true, "product must have a price"],
+  },
+  ratting: {
+    type: Number,
+    default: 4.5,
+  },
+  description: {
+    type: String,
+    // required: [true, 'product must have a description']
+  },
+  image: {
+    type: String,
+    required: [true, "product must have a image"],
+  },
+  imgUrl: {
+    type: String,
+  },
+  brandName: {
+    type: String,
+  },
+  createdAT: {
+    type: Date,
+    default: Date.now(),
+  },
+  brandId: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Model",
     },
-    price:{
-        type: Number,
-        required: [true, 'product must have a price']
-    },
-    ratting:{
-        type: Number,
-        default: 4.5
-    },
-    description:{
-        type: String,
-        // required: [true, 'product must have a description']
-    },
-    image:{
-        type: String,
-        required: [true, 'product must have a image']
-    },
-    imgUrl: {
-        type: String
-    },
-    brandName:{
-        type: String
-    },
-    created_at: {
-        type: Date,
-        default: Date.now()
-    }
+  ],
 });
 
-const Product = mongoose.model('Product', productSchema);
+const Product = mongoose.model("Product", productSchema);
 module.exports = Product;
